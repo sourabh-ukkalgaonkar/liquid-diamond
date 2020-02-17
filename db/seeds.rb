babaef07ccc3bc20d3a7fe3d0f6fe2a46a811fb2
Sourabh Ukkalgaonkar <sourabh.ukkalgaonkar@gmail.com>
2020-02-18 00:42:35 +0530

    Modified the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(
+  name: "Tester",
+  email: "tester@yopmail.com",
+  password: '123456',
+  password_confirmation: '123456'
+)
 
 (0..1000).each do
   Product.create(
